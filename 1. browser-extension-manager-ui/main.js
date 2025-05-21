bf26be7a9e2ae01683b78b4ddf883fdcc7dff1a5
شاهین شیروانی <shahin.shirvani@samim.net>
2025-05-21 13:42:25 +0330

    update

--- 1. browser-extension-manager-ui/main.js
+++ 1. browser-extension-manager-ui/main.js
@@ -2,19 +2,23 @@ const container = document.getElementById("extensions-list");
 data.forEach((item) => {
   const card = document.createElement("div");
   card.innerHTML = `
-  <div class='card-header'> 
-    <img src="${item.logo}" alt="${item.name}" class="logo">
-    <h3 class="name">${item.name}</h3>
-  </div> 
-  <p>${item.description}</p>
-
-  <label class="toggle-switch">
-    <input type="checkbox" ${item.isActive ? "checked" : ""}>
-    <span class="slider"></span>
-  </label>
-
-  <button class="remove-btn">Remove</button>
-`;
+    <div class="card">
+      <div class="card-header">
+        <img src="${item.logo}" alt="${item.name}" class="logo">
+        <div class="card-text"> 
+          <h3 class="name">${item.name}</h3>
+          <p>${item.description}</p>
+        </div>
+      </div>
+      <div class="card-footer">
+        <label class="toggle-switch">
+          <input type="checkbox" ${item.isActive ? "checked" : ""}>
+          <span class="slider"></span>
+        </label>
+      <button class="remove-btn">Remove</button>
+      </div>
+    </div>
+  `;
 
   const toggle = card.querySelector('input[type="checkbox"]');
   toggle.addEventListener("change", () => {
